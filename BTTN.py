b9480e61edc6a328123fc09b9dc7a1fefffc8bab
jayarnim <116495744+jayarnim@users.noreply.github.com>
2025-04-19 22:05:39 +0900

    Update BTTN.py

--- BTTN.py
+++ BTTN.py
@@ -4,75 +4,109 @@ import torch.nn.functional as F
 
 
 class Module(nn.Module):
-    def __init__(self, dim, prob_norm='softmax', sigma=0.5, temp=1.0):
+    def __init__(self, dim, prob_norm='softmax', sigma=0.5, temp=1.0, dropout=0.2):
         super().__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.dim = dim
         self.prob_norm = prob_norm
         self.temp = temp
 
-        # layers
-        self.W_q = nn.Linear(dim, dim)
-        self.W_k = nn.Linear(dim, dim)
-        self.W_v = nn.Linear(dim, dim)
-
+        # Prior
         self.mu_prior_layer = nn.Sequential(
             nn.Linear(dim, dim),
             nn.ReLU(),
             nn.Linear(dim, 1)
         )
+        self.logvar_prior = torch.log(torch.tensor(sigma))
 
-        # learnable log sigma
-        self.log_sigma_prior = torch.log(torch.tensor(sigma))
-        self.log_sigma_posterior = torch.log(torch.tensor(sigma))
+        # Posterior
+        self.mlp = nn.Sequential(
+            nn.Linear(dim * 2, dim),
+            nn.LayerNorm(dim),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            
+            nn.Linear(dim, dim // 2)
+        )
+        self.mu_posterior_layer = nn.Linear(dim // 2, 1)
+        self.logvar_posterior_layer = nn.Linear(dim // 2, 1)
+
+        # Context Normalization
+        self.norm = nn.LayerNorm(dim)
 
     def forward(self, Q, K, V, mask=None):
-        Q_proj = self.W_q(Q)  # (n_query, dim)
-        K_proj = self.W_k(K)
-        V_proj = self.W_v(V)
-
         if K.dim() == 2 and V.dim() == 2:
-            context, kl = self._shared_keys(Q_proj, K_proj, V_proj, mask)
+            context, kl = self._shared_keys(Q, K, V, mask)
         elif K.dim() == 3 and V.dim() == 3:
-            context, kl = self._per_query_keys(Q_proj, K_proj, V_proj, mask)
+            context, kl = self._per_query_keys(Q, K, V, mask)
         else:
             raise ValueError("Invalid K/V dimensions")
 
-        return context, kl
+        return self.norm(context), kl
 
-    def _shared_keys(self, Q_proj, K_proj, V_proj, mask):
-        mu_prior, sigma_prior = self._prior(K_proj)
-        mu_posterior, sigma_posterior = self._posterior(Q_proj, K_proj, True)
+    def _shared_keys(self, Q, K, V, mask):
+        n_query = Q.size(0)
+        n_key = K.size(0)
+
+        Q_exp = Q.unsqueeze(1).expand(-1, n_key, -1)
+        K_exp = K.unsqueeze(0).expand(n_query, -1, -1)
+
+        mu_prior, sigma_prior = self._prior(K_exp)
+        mu_posterior, sigma_posterior = self._posterior(Q_exp, K_exp)
         kl = self._kl_divergence(mu_prior, sigma_prior, mu_posterior, sigma_posterior)
 
         eps = torch.randn_like(mu_posterior)
-        samples = torch.exp(mu_posterior - 0.5 * sigma_posterior**2 + sigma_posterior * eps) / self.temp
+        samples = torch.exp(mu_posterior + sigma_posterior * eps) / self.temp
 
         if mask is not None:
             mask = self._match_mask_dim(mask, samples)
             samples = samples.masked_fill(mask, float('-inf'))
 
         weights = self._prob_norm(samples)
-        context = torch.matmul(weights, V_proj)
+        context = torch.matmul(weights, V)
         return context, kl
 
-    def _per_query_keys(self, Q_proj, K_proj, V_proj, mask):
-        Q_exp = Q_proj.unsqueeze(1)
+    def _per_query_keys(self, Q, K, V, mask):
+        Q_exp = Q.unsqueeze(1)  # (batch, 1, dim)
 
-        mu_prior, sigma_prior = self._prior(K_proj)
-        mu_posterior, sigma_posterior = self._posterior(Q_exp, K_proj, False)
+        mu_prior, sigma_prior = self._prior(K)
+        mu_posterior, sigma_posterior = self._posterior(Q_exp.expand_as(K), K)
         kl = self._kl_divergence(mu_prior, sigma_prior, mu_posterior, sigma_posterior)
 
         eps = torch.randn_like(mu_posterior)
-        samples = torch.exp(mu_posterior - 0.5 * sigma_posterior**2 + sigma_posterior * eps) / self.temp
+        samples = torch.exp(mu_posterior + sigma_posterior * eps) / self.temp
 
         if mask is not None:
             mask = self._match_mask_dim(mask, samples)
             samples = samples.masked_fill(mask, float('-inf'))
 
         weights = self._prob_norm(samples)
-        context = torch.bmm(weights.unsqueeze(1), V_proj).squeeze(1)
+        context = torch.bmm(weights.unsqueeze(1), V).squeeze(1)
         return context, kl
+
+    def _prior(self, K):
+        sigma_prior = torch.exp(self.logvar_prior)
+        mu_prior = self.mu_prior_layer(K).squeeze(-1)
+        mu_prior = mu_prior - 0.5 * sigma_prior**2
+        return mu_prior, sigma_prior
+
+    def _posterior(self, Q, K):
+        QK_cat = torch.cat([Q, K], dim=-1)
+
+        latent_vector = self.mlp(QK_cat)
+        logvar = self.logvar_posterior_layer(latent_vector).squeeze(-1)
+        phi = self.mu_posterior_layer(latent_vector).squeeze(-1)
+
+        sigma = torch.exp(0.5 * logvar)
+        mu = phi - 0.5 * sigma**2
+
+        return mu, sigma
+
+    def _kl_divergence(self, mu_prior, sigma_prior, mu_posterior, sigma_posterior):
+        term_0 = torch.log(sigma_prior / sigma_posterior)
+        term_1 = (sigma_posterior ** 2 + (mu_posterior - mu_prior) ** 2) / (2 * sigma_prior ** 2)
+        kl = (term_0 + term_1 - 0.5).mean()
+        return kl
 
     def _prob_norm(self, samples):
         if self.prob_norm == 'simplex':
@@ -80,27 +114,6 @@ class Module(nn.Module):
         else:
             weights = F.softmax(samples, dim=-1)
         return weights
-
-    def _prior(self, K):
-        sigma_prior = torch.exp(self.log_sigma_prior)
-        mu_prior = self.mu_prior_layer(K).squeeze(-1)
-        mu_prior = mu_prior - 0.5 * sigma_prior**2
-        return mu_prior, sigma_prior
-
-    def _posterior(self, Q, K, share):
-        sigma_posterior = torch.exp(self.log_sigma_posterior)
-        if share == True:
-            phi = torch.matmul(Q, K.T) / (self.dim ** 0.5)
-        else:
-            phi = torch.matmul(Q, K.transpose(-2, -1)).squeeze(1) / (self.dim ** 0.5)
-        mu_posterior = phi - 0.5 * sigma_posterior**2
-        return mu_posterior, sigma_posterior
-
-    def _kl_divergence(self, mu_prior, sigma_prior, mu_posterior, sigma_posterior):
-        term_0 = torch.log(sigma_prior / sigma_posterior)
-        term_1 = (sigma_posterior ** 2 + (mu_posterior - mu_prior) ** 2) / (2 * sigma_prior ** 2)
-        kl = (term_0 + term_1 - 0.5).mean()
-        return kl
 
     def _match_mask_dim(self, mask, target_tensor):
         while mask.ndim < target_tensor.ndim:
