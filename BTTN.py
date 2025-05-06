ab7a180950ba97581bfc00815a0d9bb9f4a3c76c
jayarnim <116495744+jayarnim@users.noreply.github.com>
2025-05-06 10:16:05 +0900

    Update BTTN.py

--- BTTN.py
+++ BTTN.py
@@ -1,69 +1,108 @@
+from typing import Literal
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Module(nn.Module):
     def __init__(
-        self, 
-        dim: int, 
-        sigma: float=0.1, 
-        gamma: float=2.0,
+        self,
+        dim: int,
+        n_heads: int, 
+        simplex_type: Literal['linear', 'exp']='linear',
+        sigma: float=0.25, 
+        tau: float=2.0, 
+        beta: float=0.25,
+        dropout: float=0.2,
     ):
         super().__init__()
+        assert dim % n_heads == 0, "dim must be divisible by n_heads"
+
         self.dim = dim
+        self.n_heads = n_heads
+        self.head_dim = dim // n_heads
+        self.simplex_type = simplex_type
         self.sigma = sigma
-        self.gamma = gamma
+        self.tau = tau
+        self.beta = beta
+        self.dropout = dropout
 
         self._init_layers()
 
     def forward(self, Q, K, V, padding=None, mask=None):
-        Q_proj = self.W_q(Q).unsqueeze(1)   # (n_query, 1, dim)
-        K_proj = self.W_k(K)                # (n_query, n_key, dim)
-        V_proj = self.W_v(V)                # (n_query, n_key, dim)
+        Q_proj = self.W_q(Q).view(Q.size(0), self.n_heads, self.head_dim).unsqueeze(2)  # (n_query, n_heads, 1, head_dim)
+        K_proj = self.W_k(K).view(K.size(0), K.size(1), self.n_heads, self.head_dim).transpose(1, 2)  # (n_query, n_heads, n_key, head_dim)
+        V_proj = self.W_v(V).view(V.size(0), V.size(1), self.n_heads, self.head_dim).transpose(1, 2)  # (n_query, n_heads, n_key, head_dim)
 
-        prior_mu, prior_sigma = self._prior(K_proj)
-        posterior_mu, posterior_sigma = self._posterior(Q_proj, K_proj)
+        samples, params = self._sampling_from_approx(Q_proj, K_proj, padding)
+
+        if padding is not None:
+            samples = torch.masked_fill(samples, self._match_dim(padding, samples), float('-inf'))
+
+        if mask is not None:
+            samples = torch.masked_fill(samples, self._match_dim(mask, samples), float('-inf'))
+
+        weights = self._simplex_projection_fn(samples)  # (n_query, n_heads, n_key)
+        head_contexts = torch.einsum('bhk,bhkd->bhd', weights, V_proj)  # (n_query, n_heads, head_dim)
+
+        # Concat and linear projection
+        flat_contexts = head_contexts.reshape(Q.size(0), self.dim)  # (n_query, dim)
+        fusion_context = self.W_o(flat_contexts)
+
+        # Pre-normalization
+        fusion_context = self.layer_norm(fusion_context)
+        fusion_context += Q
+
+        return fusion_context, params
+
+    def _sampling_from_approx(self, Q, K, padding):
+        prior_mu, prior_sigma = self._prior(K)
+        posterior_mu, posterior_sigma = self._posterior(Q, K)
+
+        eps = torch.randn_like(posterior_mu)
+        samples = torch.exp(posterior_mu + posterior_sigma * eps)
 
         params = dict(
             prior_mu=prior_mu,
             prior_sigma=prior_sigma,
             posterior_mu=posterior_mu,
             posterior_sigma=posterior_sigma,
-            padding=padding,
+            padding=self._match_dim(padding, posterior_mu),
         )
 
-        eps = torch.randn_like(posterior_mu)
-        samples = torch.exp(posterior_mu + posterior_sigma * eps)
+        return samples, params
 
-        if mask is not None:
-            samples = samples.masked_fill(self._match_dim(mask, samples), float('-inf'))
-        if padding is not None:
-            samples = samples.masked_fill(self._match_dim(padding, samples), float('-inf'))
-
-        weights = self._linear_simplex_projection_fn(samples)
-
-        context = torch.bmm(self.weights.unsqueeze(1), V_proj).squeeze(1)  # (n_query, dim)
-        context = self.layer_norm(context) + Q_proj.squeeze(1)
-
-        return context, params
+    def _simplex_projection_fn(self, samples):
+        if self.simplex_type == "linear":
+            return self._smoothed_linear_projection_fn(samples)
+        elif self.simplex_type == "exp":
+            return self._smoothed_exp_projection_fn(samples)
+        else:
+            raise ValueError("simplex type must be linear or exp")
 
     def _prior(self, K):
         sigma = torch.exp(0.5 * self.prior_logvar)
         phi = self.prior_phi(K).squeeze(-1)
-        mu = phi - 0.5 * sigma ** 2
+        mu = phi - 0.5 * (sigma ** 2)
         return mu, sigma
 
     def _posterior(self, Q, K):
         sigma = torch.exp(0.5 * self.posterior_logvar)
-        phi = (Q.expand_as(K) * K).sum(dim=-1) / (self.dim ** 0.5)
-        mu = phi - 0.5 * sigma ** 2
+        phi = (Q.expand_as(K) * K).sum(dim=-1) / (self.head_dim ** 0.5)
+        mu = phi - 0.5 * (sigma ** 2)
         return mu, sigma
 
-    def _linear_simplex_projection_fn(self, samples):
-        samples = F.relu(samples)
-        samples_power = torch.pow(samples, self.gamma)
-        weights = samples_power / (samples_power.sum(dim=-1, keepdim=True) + 1e-8)
-        return weights
+    def _smoothed_linear_projection_fn(self, samples):
+        numerator = F.relu(samples) ** self.tau
+        numerator_sum = numerator.sum(dim=-1, keepdim=True) + 1e-8
+        denominator = numerator_sum ** self.beta
+        return numerator / denominator
+
+    def _smoothed_exp_projection_fn(self, samples):
+        numerator = torch.exp(samples / self.tau)
+        numerator_sum = numerator.sum(dim=-1, keepdim=True)
+        denominator = numerator_sum ** self.beta
+        return numerator / denominator
 
     def _match_dim(self, source, target):
         while source.ndim < target.ndim:
@@ -74,20 +113,21 @@ class Module(nn.Module):
         self.W_q = nn.Linear(self.dim, self.dim)
         self.W_k = nn.Linear(self.dim, self.dim)
         self.W_v = nn.Linear(self.dim, self.dim)
+        self.W_o = nn.Linear(self.dim, self.dim)
+        self.layer_norm = nn.LayerNorm(self.dim)
 
         self.prior_phi = nn.Sequential(
-            nn.Linear(self.dim, self.dim),
-            nn.Softplus(),
-            nn.Linear(self.dim, 1),
+            nn.Linear(self.head_dim, self.head_dim),
+            nn.LayerNorm(self.head_dim),
+            nn.ReLU(),
+            nn.Dropout(self.dropout),
+            nn.Linear(self.head_dim, 1),
         )
-
         self.register_buffer(
             name='prior_logvar', 
-            tensor=torch.tensor(2 * torch.log(torch.tensor(self.sigma)))
+            tensor=torch.tensor(2 * torch.log(torch.tensor(self.sigma))),
         )
         self.register_buffer(
             name='posterior_logvar', 
-            tensor=torch.tensor(2 * torch.log(torch.tensor(self.sigma)))
+            tensor=torch.tensor(2 * torch.log(torch.tensor(self.sigma))),
         )
-
-        self.layer_norm = nn.LayerNorm(self.dim)
